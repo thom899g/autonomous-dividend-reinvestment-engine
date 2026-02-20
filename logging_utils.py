d96e610af01a0e844e668788e169460ae0753155
thom899g <153515950+thom899g@users.noreply.github.com>
2026-02-20 06:46:08 +0100

    Automated commit by HiveMind

---
+++ logging_utils.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logging():
+    """Setup basic logging configuration."""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('dividend_reinvestment.log'),
+            logging.StreamHandler()
+        ]
+    )
